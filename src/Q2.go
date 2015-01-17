38f281beb652d9d79590214e1237b50a91cf6bdc
konjoot <konjoot@gmail.com>
2015-01-17 22:23:51 +0300

    Q2.3 example

--- src/Q2.go
+++ src/Q2.go
@@ -19,4 +19,14 @@ func main(){
     goto Here
   }
 
+  fmt.Printf("For loop with array:\n")
+
+  var array [10]int
+
+  for i := 0; i < 10; i++ {
+    array[i] = i
+  }
+
+  fmt.Printf("%d\n", array)
+
 }
