7176a360db32b61001e2e94ec366145d07177b5a
konjoot <konjoot@gmail.com>
2015-01-17 22:10:57 +0300

    Q2.2 example

--- src/Q2.go
+++ src/Q2.go
@@ -3,8 +3,20 @@ import  "fmt"
 
 func main(){
 
+  fmt.Printf("For loop:\n")
   for i := 0; i < 10; i++ {
     fmt.Printf("%d\n", i)
+  }
+
+
+  fmt.Printf("Goto loop:\n")
+  i := 0
+  Here: fmt.Printf("%d\n", i)
+
+  i++
+
+  if (i < 10) {
+    goto Here
   }
 
 }
